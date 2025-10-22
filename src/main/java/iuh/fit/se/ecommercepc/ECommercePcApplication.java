81ed829312f759fd3f9e144a88f2668e32b6c8f1
MinhHung_T4TEK <dominhhung04032003@gmail.com>
2025-10-22 18:14:00 +0700

    First commit: Init Project

---
+++ src/main/java/iuh/fit/se/ecommercepc/ECommercePcApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.ecommercepc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercePcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommercePcApplication.class, args);
+    }
+
+}
